aa5c78e4e497ffb24439e938330e2cccb0f080e1
WHISKY98 <76744134+WHISKY98@users.noreply.github.com>
2024-03-23 14:27:57 +0000

    modified:   index.php 	modified:   style.css

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>NerveGear</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <nav>
+        <div>
+            <img src="" alt="">
+            NerveGear
+        </div>
+
+        <ul>
+            <li>Home</li>
+            <li>All Products</li>
+            <li>About Us</li>
+            <li>Creators</li>
+        </ul>
+
+        <div>
+            <a href="">Login</a>
+            <a href="">Sign Us</a>
+        </div>
+    </nav>
 </body>
 </html>
