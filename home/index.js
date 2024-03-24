97fae5274f0a1eaa6972f9edc6b4aede2adca021
alanWaivy <alanwaivy@gmail.com>
2024-03-24 20:38:36 +0000

    change

--- home/index.js
+++ home/index.js
@@ -25,3 +25,22 @@ function nextSlide(){
 }
 /*slide end*/
 
+let slideIndex = 0;
+        
+          function showSlides() {
+            const slides1 = document.querySelectorAll('.slide1');
+            if (slideIndex >= slides1.length) {
+              slideIndex = 0;
+            } else if (slideIndex < 0) {
+              slideIndex = slides.length - 1;
+            }
+            const offset = -slideIndex * 100;
+            document.querySelector('.slide-container').style.transform = `translateX(${offset}%)`;
+          }
+        
+          function moveSlide(n) {
+            slideIndex += n;
+            showSlides();
+          }
+        
+          showSlides();
