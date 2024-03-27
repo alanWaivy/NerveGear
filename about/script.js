7f01d3d9c184e9227ec6966ae8350a6e0792783d
sliiiima <ssalita70@gmail.com>
2024-03-27 16:20:42 +0000

    ydhudhiljmoipo

---
+++ about/script.js
@@ -0,0 +1,80 @@
+ /*QA */
+
+ document.getElementById('q01').addEventListener('click', function() {
+  var div2 = document.getElementById('a01');
+  var icon = document.getElementById('icon01');
+  if (div2.style.display === 'none') {
+    div2.style.display = 'block';
+    div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+    icon.style.transform = 'rotate(45deg)';
+    
+    
+  } else {
+    div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
+        icon.style.transform = 'rotate(0deg)';
+
+    setTimeout(() => {
+      div2.style.display = 'none';
+    }, 200); // Delay the hiding after animation
+  }
+});
+
+document.getElementById('q02').addEventListener('click', function() {
+  var div2 = document.getElementById('a02');
+  var icon = document.getElementById('icon02');
+  if (div2.style.display === 'none') {
+    div2.style.display = 'block';
+    div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+    icon.style.transform = 'rotate(45deg)';
+    
+    
+  } else {
+    div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
+        icon.style.transform = 'rotate(0deg)';
+
+    setTimeout(() => {
+      div2.style.display = 'none';
+    }, 200); // Delay the hiding after animation
+  }
+});
+
+document.getElementById('q03').addEventListener('click', function() {
+  var div2 = document.getElementById('a03');
+  var icon = document.getElementById('icon03');
+  if (div2.style.display === 'none') {
+    div2.style.display = 'block';
+    div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+    icon.style.transform = 'rotate(45deg)';
+    
+    
+  } else {
+    div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
+        icon.style.transform = 'rotate(0deg)';
+
+    setTimeout(() => {
+      div2.style.display = 'none';
+    }, 200); // Delay the hiding after animation
+  }
+});
+
+
+document.getElementById('q04').addEventListener('click', function() {
+  var div2 = document.getElementById('a04');
+  var icon = document.getElementById('icon04');
+  if (div2.style.display === 'none') {
+    div2.style.display = 'block';
+    div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+    icon.style.transform = 'rotate(45deg)';
+    
+    
+  } else {
+    div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
+        icon.style.transform = 'rotate(0deg)';
+
+    setTimeout(() => {
+      div2.style.display = 'none';
+    }, 200); // Delay the hiding after animation
+  }
+});
+
+
