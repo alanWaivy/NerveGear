81ad56dd1e37654e6ad9bcdaa89077dc70454971
alanWaivy <alanwaivy@gmail.com>
2024-04-02 06:33:38 +0000

    change

--- home/index.js
+++ home/index.js
@@ -51,80 +51,72 @@ let slideIndex = 0;
         document.getElementById('q01').addEventListener('click', function() {
           var div2 = document.getElementById('a01');
           var icon = document.getElementById('icon01');
-          if (div2.style.display === 'none') {
+          var q = document.getElementById('q1');
+          if (div2.style.display == 'none') {
             div2.style.display = 'block';
-            div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+            div2.style.animation = 'scale-up-ver-top 0.4s linear';
             icon.style.transform = 'rotate(45deg)';
-            
-            
+            q.style.height='194px';
           } else {
-            div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
-                icon.style.transform = 'rotate(0deg)';
-        
-            setTimeout(() => {
-              div2.style.display = 'none';
-            }, 200); // Delay the hiding after animation
+            q.style.height='52px';
+            div2.style.animation = 'scale-up-ver-top-reverse 0.4s linear';
+            icon.style.transform = 'rotate(0deg)';
+            setTimeout(() => {div2.style.display = 'none'}, 400); // Delay the hiding after animation
           }
         });
-
+        
         document.getElementById('q02').addEventListener('click', function() {
           var div2 = document.getElementById('a02');
           var icon = document.getElementById('icon02');
-          if (div2.style.display === 'none') {
+          var q = document.getElementById('q2');
+          if (div2.style.display == 'none') {
             div2.style.display = 'block';
-            div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+            div2.style.animation = 'scale-up-ver-top 0.4s linear';
             icon.style.transform = 'rotate(45deg)';
-            
-            
+            q.style.height='194px';
           } else {
-            div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
-                icon.style.transform = 'rotate(0deg)';
-        
-            setTimeout(() => {
-              div2.style.display = 'none';
-            }, 200); // Delay the hiding after animation
+            q.style.height='52px';
+            div2.style.animation = 'scale-up-ver-top-reverse 0.4s linear';
+            icon.style.transform = 'rotate(0deg)';
+            setTimeout(() => {div2.style.display = 'none'}, 400); // Delay the hiding after animation
           }
         });
-
+        
         document.getElementById('q03').addEventListener('click', function() {
           var div2 = document.getElementById('a03');
           var icon = document.getElementById('icon03');
-          if (div2.style.display === 'none') {
+          var q = document.getElementById('q3');
+          if (div2.style.display == 'none') {
             div2.style.display = 'block';
-            div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+            div2.style.animation = 'scale-up-ver-top 0.4s linear';
             icon.style.transform = 'rotate(45deg)';
-            
-            
+            q.style.height='194px';
           } else {
-            div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
-                icon.style.transform = 'rotate(0deg)';
-        
-            setTimeout(() => {
-              div2.style.display = 'none';
-            }, 200); // Delay the hiding after animation
+            q.style.height='52px';
+            div2.style.animation = 'scale-up-ver-top-reverse 0.4s linear';
+            icon.style.transform = 'rotate(0deg)';
+            setTimeout(() => {div2.style.display = 'none'}, 400); // Delay the hiding after animation
           }
         });
-
-
+        
+        
         document.getElementById('q04').addEventListener('click', function() {
           var div2 = document.getElementById('a04');
           var icon = document.getElementById('icon04');
-          if (div2.style.display === 'none') {
+          var q = document.getElementById('q4');
+          if (div2.style.display == 'none') {
             div2.style.display = 'block';
-            div2.style.animation = 'scale-up-ver-top 0.4s ease-in-out both';
+            div2.style.animation = 'scale-up-ver-top 0.4s linear';
             icon.style.transform = 'rotate(45deg)';
-            
-            
+            q.style.height='194px';
           } else {
-            div2.style.animation = 'scale-up-ver-top-reverse 0.4s ease-in-out both';
-                icon.style.transform = 'rotate(0deg)';
-        
-            setTimeout(() => {
-              div2.style.display = 'none';
-            }, 200); // Delay the hiding after animation
+            q.style.height='52px';
+            div2.style.animation = 'scale-up-ver-top-reverse 0.4s linear';
+            icon.style.transform = 'rotate(0deg)';
+            setTimeout(() => {div2.style.display = 'none'}, 400); // Delay the hiding after animation
           }
         });
         
-
-
-     
+        
+        
+        
