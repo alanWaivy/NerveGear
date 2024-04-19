d7f940e14a45d8d47a56a009f86fc8d7c452d6d2
alanWaivy <alanwaivy@gmail.com>
2024-04-19 19:14:21 +0100

    ch

---
+++ fullWebsite/product/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>LapShop</title>
+  <link rel="stylesheet" href="style.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
+  <script src="index.js" defer></script>
+</head>
+<body>
+    
+
+<?php include("../commenParts/header.php") ?>
+
+<section class="hero">
+<div class="part02">
+  <h1>Lorem ipsum dolor sit amet</h1>
+<p>Illo, nisi architecto. Veritatis, sint maiores veniam nulla repudiandae doloribus neque reiciendis hic dolor praesentium, </p>
+<button class="button1">Add To cart</button>
+<button>Shop Now</button>
+</div>
+</section>
+
+<div class="cart ">
+    <div class="bsDot"></div>
+    <div class="bsImg">
+      <img src="/pics/img slide 1 homepage.png" alt="img">
+    </div>
+    <div class="bsTitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    <div class="bsProperties">
+      <div class="prop"><p>Gaming</p></div>
+      <div class="prop"><p>Editing</p></div>
+      <div class="prop"><p>Working</p></div>
+    </div>
+    <div class="bsButtons">
+      <button type="button" id="btn01">Add To Cart</button>
+      <button type="button" id="btn02" >Shop Now</button>
+    </div>
+  </div> 
+  <div class="cart ">
+    <div class="bsDot"></div>
+    <div class="bsImg">
+      <img src="/pics/img slide 1 homepage.png" alt="img">
+    </div>
+    <div class="bsTitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    <div class="bsProperties">
+      <div class="prop"><p>Gaming</p></div>
+      <div class="prop"><p>Editing</p></div>
+      <div class="prop"><p>Working</p></div>
+    </div>
+    <div class="bsButtons">
+      <button type="button" id="btn01">Add To Cart</button>
+      <button type="button" id="btn02" >Shop Now</button>
+    </div>
+  </div> 
+  <div class="cart ">
+    <div class="bsDot"></div>
+    <div class="bsImg">
+      <img src="/pics/img slide 1 homepage.png" alt="img">
+    </div>
+    <div class="bsTitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    <div class="bsProperties">
+      <div class="prop"><p>Gaming</p></div>
+      <div class="prop"><p>Editing</p></div>
+      <div class="prop"><p>Working</p></div>
+    </div>
+    <div class="bsButtons">
+      <button type="button" id="btn01">Add To Cart</button>
+      <button type="button" id="btn02" >Shop Now</button>
+    </div>
+  </div> 
+  <div class="cart ">
+    <div class="bsDot"></div>
+    <div class="bsImg">
+      <img src="/pics/img slide 1 homepage.png" alt="img">
+    </div>
+    <div class="bsTitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    <div class="bsProperties">
+      <div class="prop"><p>Gaming</p></div>
+      <div class="prop"><p>Editing</p></div>
+      <div class="prop"><p>Working</p></div>
+    </div>
+    <div class="bsButtons">
+      <button type="button" id="btn01">Add To Cart</button>
+      <button type="button" id="btn02" >Shop Now</button>
+    </div>
+  </div> 
+
+
+  
+  <div class="cart ">
+    <div class="bsDot"></div>
+    <div class="bsImg">
+      <img src="/pics/img slide 1 homepage.png" alt="img">
+    </div>
+    <div class="bsTitle"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
+    <div class="bsProperties">
+      <div class="prop"><p>Gaming</p></div>
+      <div class="prop"><p>Editing</p></div>
+      <div class="prop"><p>Working</p></div>
+    </div>
+    <div class="bsButtons">
+      <button type="button" id="btn01">Add To Cart</button>
+      <button type="button" id="btn02" >Shop Now</button>
+    </div>
+  </div> 
+
+
+
+
+  <?php include("../commenParts/QA-Part.php") ?>
+
+
+  <?php include("../commenParts/QA-Part.php") ?>
+
+    
+</body>
+</html>
