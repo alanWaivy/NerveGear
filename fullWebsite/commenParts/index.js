ca62ee2ff8188a71e7672356cd98990454014898
alanWaivy <alanwaivy@gmail.com>
2024-04-21 13:13:11 +0100

    ch

--- fullWebsite/commenParts/index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.navTrigger').forEach(function(navTrigger) {
-    navTrigger.addEventListener('click', function() {
-        this.classList.toggle('active');
-        console.log("Clicked menu");
-        var mainListDiv = document.getElementById('mainListDiv');
-        mainListDiv.classList.toggle('show_list');
-        //mainListDiv.style.display = 'block'; // Ensure the div is visible
-    });
-});
